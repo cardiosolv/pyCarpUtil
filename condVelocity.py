0331b70ddebef210cdc65589a99291564146f0ad
rocha <rocha@5cc90c60-49d1-11dd-ba04-2bd1573a0c58>
2008-07-04 14:27:41 +0000

    git-svn-id: http://core.meduni-graz.at/pyUtils@2 5cc90c60-49d1-11dd-ba04-2bd1573a0c58

---
+++ condVelocity.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+"""
+Function to calculate the conduction velocity in a cable slab
+
+Example:
+        0 1 2 3 4 5 6 7 8
+            A       B
+
+@author Bernardo Martins Rocha
+"""
+
+from numpy import *
+from scipy.io import write_array
+from scipy.io import read_array
+
+from read_array_pts import read_array_pts
+
+import pdb
+
+def condVelocity(ptsFile, actFile):
+    
+    xyz = read_array_pts(ptsFile)
+    act = read_array(actFile)
+    actTeste = act
+       
+    distA =   max(xyz[:,0]/4)
+    distB = 3*max(xyz[:,0]/4)
+
+    # node A
+    tmp   = abs(xyz[:,0] - distA)
+    tmpID = where(tmp == tmp.min())    # where return tuple
+    aux   = tmpID[0][0] + 1
+    nodeA = xyz[aux]
+    actTI = where(act[:,0] == aux)     # activation time index
+    
+    if size(actTI) == 0:
+        print "  Node A was not activated. CV is unknown!"
+        return
+    else:
+        actTI = actTI[0][0]            # from tuple to scalar
+        actTA = act[actTI , 1]         # activation time for node A    
+                    
+    # node B
+    tmp   = abs(xyz[:,0] - distB)
+    tmpID = where(tmp == tmp.min())    # where return tuple
+    aux   = tmpID[0][0] + 1
+    nodeB = xyz[aux]
+    actTI = where(act[:,0] == aux)     # activation time index
+    
+    if size(actTI) == 0:
+        print "  Node B was not activated. CV is unknown!"
+        return
+    else:
+        actTI = actTI[0][0]            # from tuple to scalar
+        actTB = act[actTI , 1]         # activation time for node B
+
+    distAB = sqrt(sum(pow(nodeB - nodeA,2)))
+    condve = distAB / (actTB - actTA)
+        
+    print " CV = %.3f m/s" % (condve/1000)
+    return (condve/1000)
