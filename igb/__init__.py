139d695bc4ef59db555be2240c49bc8044c9f356
rocha <rocha@5cc90c60-49d1-11dd-ba04-2bd1573a0c58>
2008-07-15 12:54:02 +0000

    git-svn-id: http://core.meduni-graz.at/pyUtils@11 5cc90c60-49d1-11dd-ba04-2bd1573a0c58

---
+++ igb/__init__.py
@@ -0,0 +1,8 @@
+"""
+Package to read/write IGB binary format files
+
+igb_header     >>> Status: OK
+read_igb_file  >>> Status: OK
+write_igb_file >>> Status: under testing
+                           it needs more tests in order to confirm that it's OK
+"""
