d6d177bc8194bd9de1cf4e49bd732a45a6c4523e
rocha <rocha@5cc90c60-49d1-11dd-ba04-2bd1573a0c58>
2008-07-15 12:53:55 +0000

    git-svn-id: http://core.meduni-graz.at/pyUtils@10 5cc90c60-49d1-11dd-ba04-2bd1573a0c58

--- igb/write_igb.py
+++ igb/write_igb.py
@@ -1,66 +1,67 @@
 #!/usr/bin/env python
 
-import sys, os, pdb, time
-import numpy, stat, string
+import sys, os, time, gzip, stat, string, pdb
 
-from numpy import *
-from scipy.io.npfile import npfile
-#from scipy.io.fopen import fopen
+from numpy import zeros
 from igb_header import igb_header
 from read_igb_file import *
 
 """
-IGB write file - UNDER DEVELOPMENT
-
+IGB write file
 Bernardo M. Rocha
 """
 
-def packFloatData(data):
+def isnan(x):
+    return str(x) == 'nan'
+
+def pack_float_data(data):
     """
     pack binary data to be written in a binary file in 'little_endian' format
     data must has 2 dimensions (ndim=2)
     data(x,t) -> x node
               -> t time slice
     """
-    import struct
-    
+    import struct    
     packedData=''
     size = shape(data)
     for j in xrange(size[1]):
         for i in xrange(size[0]):
             packedData+=struct.pack('<f',data[i,j])
-
-    #packedData = ''.join([struct.pack('<f',data[i,j]) for j in xrange(size[1]) for i in xrange(size[0])])
-
     return packedData
 
-def write_igb(data, hd, filename):
+def write_igb(data, hd, filename, gzipped=True):
 
-    # start time
+    
     t0 = time.time()
     
+    if data.ndim == 2:                   # if data is (nodes, time_slices)
+        data = data[:,newaxis,newaxis,:]
+    
+    if data.ndim == 3:
+        data = data[:,:,:,newaxis]
+
     n_dims = data.ndim
-    d_data = zeros((data.ndim))    
+    d_data = zeros((data.ndim))
     for i in xrange(data.ndim):
         d_data[i] = data.shape[i]
-        
-    #if n_dims == 2:#
-    #    d_data = zeros(d)
-    #    array([d_data,1,1])
-    
-    #if n_dims == 3:
-    #    d_data = array([d_data,1])
-      
+
     t1 = time.time()
-    pData = packFloatData(data.squeeze())
+    pData = pack_float_data(data.squeeze())
     t2 = time.time()
 
     print " packFloatData: %.5f seconds wall time" % (t2-t1)
 
-    if d_data[0] == hd.x and d_data[1] == hd.y and d_data[2] == hd.z and d_data[3] == hd.t:        
-        fh = npfile (filename,'w')       
-        write_header(hd, fh)        
-        fh.write_array(pData)       
+    if d_data[0] == hd.x and d_data[1] == hd.y and d_data[2] == hd.z and d_data[3] == hd.t:
+        
+        if gzipped:
+            gzfilename = '%s.gz' % filename
+            fh = gzip.GzipFile(filename=gzfilename,mode='wb')
+        else:
+            fh = open (filename,'wb')
+
+        write_header(hd, fh)
+        
+        fh.write(pData)
         fh.close()
     else:
         print " ERROR: Header-Data mismatch\n"
@@ -78,52 +79,48 @@ def write_header(hd, fh):
     buffer = [ ' ' for i in xrange(hs-1)]
 
     # create header substrings
-    N  = 'x:%d y:%d z:%d t:%d \r\n' % (hd.x, hd.y,hd.z,hd.t)
-    DT = 'type:%s \r\n' % (hd.type)
-    D  = 'dim_x:%f dim_y:%f dim_z:%f dim_t:%f \r\n' % (hd.dim_x, hd.dim_y, hd.dim_z, hd.dim_t)
+    N  = 'x:%d y:%d z:%d t:%d' % (hd.x, hd.y,hd.z,hd.t)
+    DT = 'type:%s' % (hd.type)
+    SY = 'systeme:%s \r\n' % (hd.systeme)       
+    D  = ''
+    if (not isnan(hd.dim_x)): D += 'dim_x:%f ' % hd.dim_x
+    if (not isnan(hd.dim_y)): D += 'dim_y:%f ' % hd.dim_y
+    if (not isnan(hd.dim_z)): D += 'dim_z:%f ' % hd.dim_z
+    if (not isnan(hd.dim_t)): D += 'dim_t:%.2f' % hd.dim_t
+    #D  = 'dim_x:%f dim_y:%f dim_z:%f dim_t:%f \r\n' % (hd.dim_x, hd.dim_y, hd.dim_z, hd.dim_t)
     U  = 'unites_x:%s unites_y:%s unites_z:%s unites_t:%s unites:%s \n' % (hd.unites_x, hd.unites_y, hd.unites_z, hd.unites_t, hd.unites)
-    F  = 'facteur:%d \r\n' % (hd.facteur)
+    F  = 'facteur:%d' % (hd.facteur)
     Z  = 'zero:%d \r\n' % (hd.zero)
-    SY = 'systeme:%s \r\n' % (hd.systeme)
 
     # concatenate header substrings
-    hd_str = '%s %s %s %s %s %s %s' % (N, DT, D, U, F, Z, SY)
-    
+    hd_str = '%s %s %s %s %s %s %s' % (N, DT, SY, D, U, F, Z)
+        
     # fill rest with empty lines, each line not longer than 80 chars
     lines = int(floor( (1023 - len(hd_str) ) / 78 ))
    
     ln = [' ' for i in xrange(75)]    
     ln_str = string.join(ln, '')
-        
+    
     for i in xrange(lines):
         hd_str = '%s %s \r\n' % (hd_str, ln_str)
 
-    # convert to character array
+    # convert to character array and write to file
     for i in xrange(len(hd_str)):
         buffer[i] = hd_str[i]
     buffer_str = string.join(buffer, '')
+    fh.write(buffer_str)    
 
-    # write buffer
-    fh.write_array(buffer_str)
-    #fh.write(buffer_str)
-
-    # write header termination character
-    # (seems to be 0x0C)
+    # write header termination character (seems to be 0x0C)
     hd_term = chr(12)
-    fh.write_array(hd_term)
-    #fh.write(hd_term)
+    fh.write(hd_term)
 
 if __name__ == "__main__":
        
-    #hd = igb_header([18036,1,1,101],'float',[nan,nan,nan,100.0100],('cm', 'cm', '', 'mss', 'mV'),1,0)
-    #data = zeros((18036*101)).reshape(18036,1,1,101)
-
     # resolution 100um - UCLA_RAB model
-    igb_file = '/home/rocha/sim/experiment_gm_ucla/gm_t0100/vm.igb'
-    pts_file = '/home/rocha/sim/experiment_gm_ucla/gm_t0100/gm_t0100um_i.pts'
+    igb_file = '/data/sim/space_constant/sc_ucla_long_0100um/vm.igb'
+    pts_file = '/data/sim/space_constant/sc_ucla_long_0100um/sc_ucla_long_0100um_i.pts'
     [vm, hd] = read_igb_slice(igb_file)
     Vm = vm.squeeze()
-
-    write_igb (vm, hd, 'teste.igb')#
     
-    #pdb.set_trace()
+    print shape(Vm)
+    write_igb (Vm, hd, 'teste.igb')
