96f623f8c1d08e76cc212ca5be40d8defbb596c3
rocha <rocha@5cc90c60-49d1-11dd-ba04-2bd1573a0c58>
2009-02-23 13:55:07 +0000

    git-svn-id: http://core.meduni-graz.at/pyUtils@56 5cc90c60-49d1-11dd-ba04-2bd1573a0c58

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
 # A set of Python script for pre/post-processing with CARP
 #
-# export PYTHONPATH=$PYTHONPATH:/home/rocha/src/pyUtils/
+#export PYTHONPATH=$PYTHONPATH:/home/rocha/src/pyUtils/
+#export PATH=$PATH:/home/rocha/src/pyUtils
