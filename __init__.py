5f84fb5cb1f7fd7cf25d9ee4737e17aaeb61efd3
rocha <rocha@5cc90c60-49d1-11dd-ba04-2bd1573a0c58>
2009-06-15 07:06:30 +0000

    git-svn-id: http://core.meduni-graz.at/pyUtils@73 5cc90c60-49d1-11dd-ba04-2bd1573a0c58

--- __init__.py
+++ __init__.py
@@ -1,4 +1,14 @@
-# A set of Python script for pre/post-processing with CARP
+# ##############################################################################
+# A set of Python scripts for CARP pre- and post-processing
 #
+# Essentially, the Cardiac Arrhythmia Research Package (CARP) is a 
+# multi-purpose bidomain simulator. CARP consists of several sub-packages
+# which are dedicated to specific tasks like bidomain (computational core), 
+# LIMPET (ionic model library), visualization of structured and unstrctured 
+# grids (Meshalyzer).
+#
+# Medical University of Graz, 2009
+# ##############################################################################
+
 #export PYTHONPATH=$PYTHONPATH:/home/rocha/src/pyUtils/
 #export PATH=$PATH:/home/rocha/src/pyUtils
