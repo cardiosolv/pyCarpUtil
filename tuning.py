d5111be1246b41add6b4f2e7f1ecc49de9c45ede
rocha <rocha@5cc90c60-49d1-11dd-ba04-2bd1573a0c58>
2009-06-15 07:05:26 +0000

    git-svn-id: http://core.meduni-graz.at/pyUtils@69 5cc90c60-49d1-11dd-ba04-2bd1573a0c58

--- tuning.py
+++ tuning.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
-import os, sys, getopt
-from carptools.msh_file import MeshFile
-from carptools.par_file import ParameterFile
+import os, sys, getopt, shutil
+from carptools.MeshFile import MeshFile
+from carptools.ParFile import ParameterFile
 from condVelocity import condVelocity
 from sctools import check_path, run_command_line
 
@@ -18,19 +18,21 @@ class CableTest(ParameterFile):
     Base class for the cable to run the tuning simulations
     """
     def __init__(self, im, gil, gel, beta, vs):
+
         ParameterFile.__init__(self, ionicModel=im, carp_ver=vs)
 
         self.set_parameter('surfvolrat',  beta)
-        self.set_parameter('solnmethod',  4)
         self.set_parameter('gridout_i',   2)
         self.set_parameter('dt',          10)
         self.set_parameter('timedt',      0.5)
-        self.set_parameter('spacedt',     1)
-        self.set_parameter('CN_parab',    1)
+        self.set_parameter('spacedt',     1)        
+        self.set_parameter('parab_solve', 1) # old CARPm (CN_parab)
+        self.set_parameter('bidomain',    0) # old CARPm (solnmethod)
         self.set_parameter('cg_tol_parab',1.0e-06)
+        self.set_parameter('tend',        200)
+
         #self.set_parameter('num_LATs',    1)
         #self.set_parameter('lats[0].ID',  'activation')
-        self.set_parameter('tend',        200)
 
         # version specific settings
         if vs is 'carpe':
@@ -57,9 +59,12 @@ def calculate_g(v1,v0,g0):
     """
     return g0 * ((v1/v0)*(v1/v0))
 
+# end of calculate_g
+
 def find_CV(avg_dx, gil, gel, beta, model, carpBinary, mesherBinary, carp_ver):
     """
-    Input: avg_dx - mesh resolution as a scalar or list (list - Not implemented yet)
+    Input: avg_dx - mesh resolution as a scalar or list
+                           (list - Not implemented yet)
            gil    - intracellular conductivity along the cable
            gel    - extracellular conductivity along the cable
            beta   - surface-to-volume ratio
@@ -83,43 +88,53 @@ def find_CV(avg_dx, gil, gel, beta, model, carpBinary, mesherBinary, carp_ver):
 
     # settings
     xsize = 1.0 # cm
+    mesh_temp = "mesh4exp"
     mesh_name = "%s/mesh4exp"     % (temp_dir)
     mesh_file = "%s/expMesh.par"  % (temp_dir)
     carp_file = "%s/expParam.par" % (temp_dir)
-    outp_dir = "%s/OUTPUT_DIR"    % (temp_dir)
+    out_dir = "%s/OUTPUT_DIR"    % (temp_dir)
 
     for res in resList:
         
-        yzsize = float(res/10000.)   # cm
-           
-        my_mesh = MeshFile(mesh_name, size0=xsize, size1=yzsize, size2=yzsize, element=1, resolution=res)
-        my_mesh.write_to_file(mesh_file)
+        yzsize = float(res/10000.) # convert to cm
 
-        my_cable = CableTest(model, gil, gel, beta, carp_ver)
-        my_cable.write_to_file(carp_ver,carp_file)
+        # create mesh and cable 
+        msh = MeshFile(mesh_name, size0=xsize, size1=yzsize,
+                       size2=yzsize, element=1, resolution=res)
+        msh.write_to_file(mesh_file)
+
+        cab = CableTest(model, gil, gel, beta, carp_ver)
+        cab.write_to_file(carp_ver, carp_file)
 
         # run MESHER
         cmd = '%s +F %s' % (mesherBinary, mesh_file)
         if DEBUG: print 'running %s' % cmd
         run_command_line(cmd)        
+
         # run CARP
-        cmd = '%s +F %s -meshname %s -simID %s'  % (carpBinary, carp_file, mesh_name, outp_dir)
+        cmd = '%s +F %s -meshname %s -simID %s'  % (carpBinary,
+                                  carp_file, mesh_name, out_dir)
         if DEBUG: print 'running %s' % cmd
         run_command_line(cmd)
-        
+
         # calculate CV
-        sim_pts = "%s/%s_i.pts" % (outp_dir, mesh_name)
-        sim_act = "%s/activation-thresh.dat" % (outp_dir)
-        cvList.append( condVelocity(sim_pts, sim_act, output=False) )
+        sim_pts = "%s/%s_i.pts" % (out_dir, mesh_temp)
+        sim_act = "%s/activation-thresh.dat" % (out_dir)
+        condvel = condVelocity(sim_pts, sim_act, output=False)
+        cvList.append(condvel)
+
+        # clean temp files
+        if os.path.isdir(out_dir):
+            shutil.rmtree(out_dir)
+        if os.path.isfile(carp_file):
+            os.remove(carp_file)
     
     if len(resList) == 1: return cvList[0]
     return cvList
 
-def run_simulation():
-    # try to implement and then use map
-    pass
-  
-def printHelp():
+# end of find_CV
+ 
+def print_help():
     print """
   >> Usage       : python -d <dx> -v <target velocity> -m <model> -i <gil> -e <gel> -b <beta>
   >> Description : script for tuning tissue conductivities\n  >> Parameters  :
@@ -133,7 +148,15 @@ def printHelp():
   \t --with-carp = <path_to_carp_binary>   \t specify your carp version
   \t --with-mesher=<path_to_mesher_binary> \t specify your mesher version
   """
- 
+
+def print_header(model, avgdx, vel, tol):
+    print "\n S i m u l a t i o n   s e t t i n g s\n"
+    print "    ionic model      :  %s" % model
+    print "    mesh resolution  :  %s" % avgdx
+    print "    CV desired       : %8.5f m/s" % (vel)
+    print "    tolerance        :  %.4f" % tol
+    print "\n S i m u l a t i n g \n"
+
 def main(argv):
 
     # default values 
@@ -147,7 +170,7 @@ def main(argv):
     checkBin     = True
     carpBinary   = 'carp.linux.petsc'
     mesherBinary = 'mesher'
-    carp_version = 'carpe'
+    carp_version = 'carpm'
     
     if len(argv) == 1:
         printHelp(); sys.exit(1)
@@ -158,7 +181,7 @@ def main(argv):
                     ["help", "resolution=","velocity=","gi=","ge=","beta=","model=","tol=","with-carp=","with-mesher="])
     except getopt.GetoptError, err:
         print str(err) # option -a not recognized"
-        printHelp()
+        print_help()
         sys.exit(-1)
     
     for o, a in opts:
@@ -198,18 +221,13 @@ def main(argv):
     eps = vel * tol    
     its = 1
     CV_measured = 0.
-    
-    print "\n S i m u l a t i o n   s e t t i n g s\n"
-    print "    ionic model      :  %s" % model
-    print "    mesh resolution  :  %s" % avgdx
-    print "    CV desired       : %8.5f m/s" % (vel)
-    print "    tolerance        :  %.4f" % tol
-    print "\n S i m u l a t i n g ...\n"
+
+    print_header (model, avgdx, vel, tol)
     
     while abs(vel - CV_measured) > eps:
         print "    Iteration  %d: gil =%8.5f, gel =%8.5f, gl_bulk =%8.5f" % (its,gil,gel,(gil*gel/gil+gel)),
         sys.stdout.flush()
-        
+
         CV_measured = find_CV (avgdx, gil, gel, beta, model, carpBinary, mesherBinary, carp_version)
         print " --->  CV measured : %8.5f m/s" % CV_measured
         
@@ -223,6 +241,8 @@ def main(argv):
     print "\n S i m u l a t i o n   r e s u l t s\n"    
     print "    bulk conductivity: %8.5f"          % (gl_bulk)
     print "    use gi/ge        : %8.5f %8.5f \n" % (gel*gl_bulk/(gel-gl_bulk), gel)
+
+# end of main
 
 if __name__ == "__main__":
     main(sys.argv)
